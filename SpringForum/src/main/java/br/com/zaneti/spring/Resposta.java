59c419a1169e8ab4775b10a7064be9d5d95aa660
rafaelz <rafaelz@NRZM01>
2016-12-03 21:21:39 -0200

    Commit inicial

---
+++ SpringForum/src/main/java/br/com/zaneti/spring/Resposta.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.zaneti.spring;
+
+/**
+ *
+ * @author rafaelz
+ */
+public class Resposta {
+
+    private Usuario usuario;
+    private Topico topico;
+
+    public Resposta(Usuario usuario, Topico topico) {
+        this.usuario = usuario;
+        this.topico = topico;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Topico getTopico() {
+        return topico;
+    }
+
+    public void setTopico(Topico topico) {
+        this.topico = topico;
+    }
+
+}
---
+++ SpringForum/src/main/java/br/com/zaneti/spring/Topico.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.zaneti.spring;
+
+/**
+ *
+ * @author rafaelz
+ */
+public class Topico {
+    
+    private Usuario usuario;
+    private Assunto assunto;
+
+    public Topico(Usuario usuario, Assunto assunto) {
+        this.usuario = usuario;
+        this.assunto = assunto;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public Assunto getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(Assunto assunto) {
+        this.assunto = assunto;
+    }
+        
+}
